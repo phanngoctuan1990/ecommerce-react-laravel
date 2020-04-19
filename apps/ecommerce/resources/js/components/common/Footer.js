687fd6fd9b00c31487de47b7163afca4dc0058a5
phanngoctuan1990 <phanngoctuan1990@gmail.com>
2020-04-19 15:04:01 +0700

    [Frontend] Update footer

--- apps/ecommerce/resources/js/components/common/Footer.js
+++ apps/ecommerce/resources/js/components/common/Footer.js
@@ -1,8 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Row, Table, Col } from "react-bootstrap";
 
 class Footer extends React.Component {
     render() {
-        return <div>Footer</div>;
+        return (
+            <footer className="footer">
+                <Navbar>
+                    <Container>
+                        <Row>
+                            <Col className="offset-lg-2">
+                                <br />
+                                <Table responsive className="table-footer">
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <Link to={"/myaccount"}>
+                                                    My Account
+                                                </Link>
+                                            </td>
+                                            <td>
+                                                <Link to={"/about"}>
+                                                    About Project
+                                                </Link>
+                                            </td>
+                                            <td>
+                                                <Link to={"/people"}>
+                                                    People
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <Link to={"/myorders"}>
+                                                    My Orders
+                                                </Link>
+                                            </td>
+                                            <td>
+                                                <Link to={"/contact"}>
+                                                    Contact
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg="12">
+                                <div className="text-center copyright-height">
+                                    Copyright &copy; {new Date().getFullYear()}
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Navbar>
+            </footer>
+        );
     }
 }
 
